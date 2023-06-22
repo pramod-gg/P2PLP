0dc5d8ebd2b9f8db45d2b2ef5957cd6f4c4d28aa
Pramod <137027509+pramod-gg@users.noreply.github.com>
2023-06-22 12:48:42 +0000

    added file upload

--- p2plp/src/main/java/com/stackroute/p2plp/service/UserService.java
+++ p2plp/src/main/java/com/stackroute/p2plp/service/UserService.java
@@ -3,6 +3,7 @@ package com.stackroute.p2plp.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.stackroute.p2plp.model.User;
@@ -20,7 +21,10 @@ public class UserService {
 
 
     public User saveUser(User user) {
-        return userRepository.save(user);
+        User newUser = new User();
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        return userRepository.save(newUser);
     }
 
     public User getUserByEmail(String email) {
